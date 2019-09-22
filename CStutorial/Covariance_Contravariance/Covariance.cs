9473e10c456ad54a9688469beea9b8f36e01d63d
zeroJava <zero_2009@hotmail.co.uk>
2019-09-23 00:28:31 +0100

    covariance notes

---
+++ CStutorial/Covariance_Contravariance/Covariance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CStutorial.Generics;
+using CStutorial.Inheritance;
+
+namespace CStutorial.Covariance_Contravariance
+{
+    public class Covariance
+    {
+        public void ConvarianceExample()
+        {
+            /*
+             * Convariance in generic means that a data-type
+             * with type-parameter with base can convert to
+             * a data-type of the same data type but with a
+             * subclass type-parameter.
+             * */
+
+            IGenericInterface<InheritClass2> eg1 = new GenericInterfaceClass2<InheritClass2>();
+            IGenericInterface<InheritClass1> converianceEg1 = eg1;
+        }
+
+        /*
+         * Covariance and contravariance means convertable
+         * by implict reference conversion (Albahari). 
+         * */
+    }
+}
--- CStutorial/Generics/GenericInterfaceClass.cs
+++ CStutorial/Generics/GenericInterfaceClass.cs
@@ -6,9 +6,30 @@ using System.Threading.Tasks;
 
 namespace CStutorial.Generics
 {
-    class GenericInterfaceClass : IGenericInterface<string>
+    public class GenericInterfaceClass : IGenericInterface<string>
     {
         public void GenericMethod(string parameter1)
+        {
+            /* The paramater has been automatically changed form
+             * type-parameter to string, becuase define the
+             * data-type of type-parameter as string when inheriting.
+             * */
+            System.Console.WriteLine("GenericMethod: " + parameter1.GetType());
+        }
+
+        public void GenericMethod2<S>(S parametr1)
+        {
+            /* This generic method inherited from the interface
+             * still has the type-parameter, because the type-parameter
+             * is connected to the method head, and not to the interface.
+             * */
+            System.Console.WriteLine("GenericMethod2: " + parametr1.GetType());
+        }
+    }
+
+    public class GenericInterfaceClass2<T> : IGenericInterface<T>
+    {
+        public void GenericMethod(T parameter1)
         {
             /* The paramater has been automatically changed form
              * type-parameter to string, becuase define the
