24e79438b9e6bc8d4e2ab2b43caa1e7caf39d9e5
zeroJava <zero_2009@hotmail.co.uk>
2019-10-30 23:44:57 +0000

    Added covariance and contravariance delegate.

---
+++ CStutorial/Delatges/DelegateContravarianceEx.cs
@@ -0,0 +1,53 @@
+﻿using CStutorial.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStutorial.Delatges
+{
+	public delegate void DelContravarianceEx(string paramOne);
+	public delegate void DelContravarianceExTwo(InterfaceExampleClass interfaceExample);
+
+	/*
+	 * Delegates also have the contravariance ability,
+	 * meaning that a method with base data-type para-
+	 * meters can be assigned to a delegate with sub-
+	 * class parameters.
+	 * */
+
+	/*
+	 * E.g.
+	 * DelContravarianceExTwo contravarianceExTwo = DelegateExampleTwo;
+	 * */
+
+	public class DelegateContravarianceEx
+	{
+		private void ExecuteExample()
+		{
+			/*
+			 * The code below demonstrates contravariance
+			 * with delegates.
+			 * 
+			 * We see delegates with specific data-types
+			 * accepting methods are whose parameters
+			 * are replaced with generic data-type belong
+			 * to the type heirachy.
+			 * */
+
+			DelContravarianceEx contravarianceEx1 = DelegateExample;
+			DelContravarianceExTwo contravarianceExTwo = DelegateExampleTwo;
+		}
+
+		private void DelegateExample(object paramOne)
+		{
+			Console.WriteLine("Example DelegateExample.");
+		}
+
+		private void DelegateExampleTwo(IExample1 interfaceExample)
+		{
+			Console.WriteLine("Example DelegateExampleTwo.");
+		}
+	}
+}
---
+++ CStutorial/Delatges/DelegateCovarianceEx.cs
@@ -0,0 +1,53 @@
+﻿using CStutorial.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStutorial.Delatges
+{
+	public delegate IExample1 DelCovarianceEx(string paramOne);
+	public delegate void DelCovarianceExTwo(IExample1 example);
+
+	/*
+	 * 
+	 * Delegates also have the covariance ability, meaning
+	 * that a method with subclass data-type parameters
+	 * can be assigned to a delegate with base class para-
+	 * meters.
+	 * */
+
+	public class DelegateCovarianceEx
+	{
+		private void ExecuteExample()
+		{
+			/*
+			 * The code below demonstrates covariance with
+			 * delegates.
+			 * 
+			 * We see delegates with base data-types accep-
+			 * ting methods are whose parameters are
+			 * assigned with specific data-type belong to the
+			 * type heirachy.
+			 * */
+
+			DelCovarianceEx contravarianceEx1 = DelegateExample;
+
+			//DelCovarianceExTwo contravarianceExTwo = DelegateExampleTwo;
+			/*
+			 * This wont work with.
+			 * */
+		}
+
+		private InterfaceExampleClass DelegateExample(object paramOne)
+		{
+			return new InterfaceExampleClass();
+		}
+
+		private void DelegateExampleTwo(InterfaceExampleClass interfaceExample)
+		{
+			Console.WriteLine("Example DelegateExampleTwo.");
+		}
+	}
+}
