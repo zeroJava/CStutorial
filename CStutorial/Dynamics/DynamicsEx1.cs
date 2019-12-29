8546bcd247b41fd243bc4cffcbbf4200bd07f80e
zeroJava <zero_2009@hotmail.co.uk>
2019-12-29 22:20:26 +0000

    chanes

--- CStutorial/Dynamics/DynamicsEx1.cs
+++ CStutorial/Dynamics/DynamicsEx1.cs
@@ -11,36 +11,45 @@ namespace CStutorial.Dynamics
         /*
          * C# is a statically typed language, meaning
          * that the type of the variable is know by the
-         * compiler at compile-time, and that once the
-         * variable has been assigned a type, it cannot
-         * changed.
+         * compiler at compile-time.
          * 
+         * Also, once the variable has been assigned a
+         * type, it cannot changed.
+         * 
+         * */
+
+        /*
+         * Static-type.
+         * 
+         * As mentioned, due to the fact that C# is
+         * a statically typed programming language,
+         * the compiler analyses the code, looking
+         * for a match, and when can't find a method
+         * that belongs to the type, then the compi-
+         * ler will throw an error.
          * */
 
         public void ExecuteDynamicExample()
         {
             /*
-             * As mentioned, due to the fact that C# is
-             * a statically typed programming language,
-             * the compiler analyses the code, and when
-             * there's something that does not make se-
-             * nce to the compiler, it will throw an
-             * compiler error.
-             * */
-
-            /*
              * In C#, we have a function that tells the
-             * compiler to ignore this variable and all
-             * the properties and function until run-
-             * time.
+             * compiler to ignore all members for this
+             * variable, and wait until run-time to bind.
              * 
-             * This is know a Dynamic-Binding.
+             * This is know a Dynamic-Binding, which is
+             * activated by using the dynamic keyword.
              * */
 
             dynamic dynamic1 = new InnerDynamicEx1();
             int number = dynamic1.GetMeANumber();
             try
             {
+                /*
+                 * Dynamic-Binding allows the compiler to
+                 * ignore the member assigned to the var-
+                 * iable, and waits for the binding to
+                 * happen at run-time.
+                 * */
                 string str = dynamic1.GetMeAString();
             }
             catch (Exception exception)
