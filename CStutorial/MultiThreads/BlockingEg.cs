d136efe248518e3396636f2d7d35190f110e7a3a
zeroJava <zero_2009@hotmail.co.uk>
2023-07-08 19:45:20 +0100

    new notes

---
+++ CStutorial/MultiThreads/BlockingEg.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CStutorial.MultiThreads
+{
+	internal static class BlockingEg
+	{
+		/*
+		 * Blocking is when a thread is paused and the processor stop processing
+		 * the remainder of the thread until at certain condition is met for it
+		 * to resume processing.
+		 * */
+
+		/*
+		 * I/O bound: operations that take requires time to finish until it can
+		 * move on.
+		 * 
+		 * Compute bound: a CPU intensive operation
+		 * */
+
+		public static void ExecuteThreadOne()
+		{
+			Console.WriteLine("Main thread started");
+			
+			int sharedCounter = 0;
+			
+			Action<string, string> tBody = (name, symbol) =>
+			{
+				Console.WriteLine($"{name} started");
+
+				/*
+				 * Any variables created within the delegates body is stored in the
+				 * threads memory stack, i.e. each thread will have it's own copy
+				 * of the c variable below. 
+				 * 
+				 * This is know as a local state.
+				 * */
+
+				/*
+				 * The above variable is stored normally on a single memory stack,
+				 * and is referenced by both thread.
+				 * 
+				 * This is a shared state.
+				 * 
+				 * The local variable captured by lambda expression, the compiler
+				 * converts them to fields during compile time so they can be 
+				 * shared. Look at notes appendix one.
+				 * 
+				 * */
+
+				for (int c = 0; c < 200; c++)
+				{
+					if (c % 10 == 0)
+					{
+						Console.WriteLine($"{symbol}{sharedCounter}{symbol}");
+						Thread.Sleep(1000);
+					}
+					sharedCounter++;
+				}
+				Console.WriteLine($"{name} finised");
+			};
+
+			Thread t1 = new Thread(() => { tBody("T1", "-"); });
+			Thread t2 = new Thread(() => { tBody("T2", "+"); });
+
+			t1.Start();
+			t2.Start();
+
+			t1.Join();
+			t2.Join();
+
+			Console.WriteLine("Main thread finished");
+		}
+	}
+}
---
+++ CStutorial/MultiThreads/LockingEg.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CStutorial.MultiThreads
+{
+	internal static class LockingEg
+	{
+		private static string currentThread = string.Empty;
+		private static readonly object exclusiveLock = new object();
+
+		/*
+		 * Locking is when the CLR prevent multiple thread from accessing the
+		 * shared resource at the same time. Once a share resource is in use,
+		 * the CLR will prevent another threads from accessing the resource the
+		 * thread has finished using it.
+		 * */
+
+		public static void ExecuteThreadOne()
+		{
+			RunExclusiveEgOne("T1");
+			RunExclusiveEgOne("T2");
+			RunExclusiveEgOne("T3");
+		}
+
+		private static void RunExclusiveEgOne(string name)
+		{
+			Thread t = new Thread(() =>
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					/*
+					 * The simplest way to attain an exlusive lock is through the
+					 * lock() statement.
+					 * 
+					 * When the lock statement is executed, the runtime will prevent
+					 * any threads from accessing the exclusiveLock variable and any
+					 * of the shared resources inside the lock body.
+					 * */
+
+					/*
+					 * When a thread executes a section of the code that is locked
+					 * via the lock() statement, the runtime will put it into a
+					 * waiting list, which then will get its turn when the current
+					 * locked thread finishes executing the locked section.
+					 * 
+					 * The lock() waiting list a first-in-first-out
+					 * */
+
+					/*
+					 * lock() statement is a simpler way of writing Monitor.Enter
+					 * and Monitor.Exit. Check notes.
+					 * */
+
+					Console.WriteLine($"{name} is entering the lock");
+					lock (exclusiveLock)
+					{
+						currentThread = name;
+						Console.WriteLine(name);
+						Thread.Sleep(2000);
+					}
+				}
+			});
+			t.Start();
+		}
+	}
+}
